ca38dd8d7478ba9380d72cd9a1e86a26c3deb550
svilen-tech <78470960+svilen-tech@users.noreply.github.com>
2021-02-21 18:19:42 +0200

    Add files via upload

---
+++ Java-Advanced/FirstHalf/StackIterator/Main.java
@@ -0,0 +1,36 @@
+package IteratorsAndComparators.StackIterator;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CustomStack customStack = new CustomStack();
+        String[] initialValues = scanner.nextLine().split(",\\s+");
+
+        customStack.push(Integer.parseInt(initialValues[0].split("\\s+")[1]));
+        for (int i = 1; i <initialValues.length ; i++) {
+            customStack.push(Integer.parseInt(initialValues[i].trim()));
+        }
+
+        String line=" ";
+        while (!"END".equals(line=scanner.nextLine())){
+            if (line.equals("Pop")){
+                try {
+                    customStack.pop();
+                }catch (IllegalStateException e){
+                    System.out.println(e.getMessage());
+                }
+            }else {
+                customStack.push(Integer.parseInt(line.split("\\s+")[1]));
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (Integer element : customStack) {
+                System.out.println(element);
+            }
+        }
+    }
+}
