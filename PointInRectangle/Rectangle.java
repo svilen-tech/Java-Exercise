13263b66318f82cd03f78207318ed4f0687a267b
svilen-tech <78470960+svilen-tech@users.noreply.github.com>
2021-02-24 18:45:06 +0200

    Add files via upload

---
+++ PointInRectangle/Rectangle.java
@@ -0,0 +1,23 @@
+package WorkingWithAbstraction.PointInRectangle;
+
+public class Rectangle {
+    private Point bottomLeft;
+    private Point topRight;
+
+    public Rectangle(Point bottomLeft, Point topRight) {
+        this.bottomLeft = bottomLeft;
+        this.topRight = topRight;
+    }
+
+    public Point getBottomLeft() {
+        return bottomLeft;
+    }
+
+    public Point getTopRight() {
+        return topRight;
+    }
+    public boolean contains(int x,int y){
+        return getBottomLeft().getX()<=x&&x<=getTopRight().getX()
+                && getBottomLeft().getY()<=y&&y<=getTopRight().getY();
+    }
+}
